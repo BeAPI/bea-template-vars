9cb11f6f6a8ccb7a2f57b60bd03c4b74827c91e6
Nicolas Kulka <kulka.nicolas@gmail.com>
2016-10-05 16:04:09 +0200

    Update credentials

--- bea-template-vars.php
+++ bea-template-vars.php
@@ -1,21 +1,28 @@
-<?php 
+<?php
 /*
  Plugin Name: BEA Template Vars
  Version: 0.0.1
- Plugin URI: https://github.com/BeAPI/bea-template-vars
+ Plugin URI: http://www.beapi.fr
  Description: Easily pass vars through a template and reuse them.
- Author: BeAPI
+ Author: BE API Technical team
  Author URI: http://www.beapi.fr
+ Domain Path: languages
+ Text Domain: bea-template-vars
+ 
  ----
- Copyright 2016 BeAPI Technical team (technique@beapi.fr)
+ 
+ Copyright 2016 BE API Technical team (human@beapi.fr)
+ 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation; either version 2 of the License, or
  (at your option) any later version.
+ 
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
+ 
  You should have received a copy of the GNU General Public License
  along with this program; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
